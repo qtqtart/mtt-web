5c2b4ecd4b70a94d5dc5cf2f8d64e938b87c71d6
artem <qtqt.art@gmail.com>
2025-02-28 00:29:31 +0300

    feat: add template project

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,51 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import simpleImportSortPlugin from "eslint-plugin-simple-import-sort";
+import prettierPlugin from "eslint-plugin-prettier";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
+import globals from "globals";
+import ts from "typescript-eslint";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default ts.config(
+  ...ts.configs.recommended,
+  js.configs.recommended,
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist", "config.js"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.es2025,
+        ...globals.browser,
+        ...globals.node,
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      prettier: prettierPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefreshPlugin,
+      "simple-import-sort": simpleImportSortPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "no-undef": "off",
+      "no-unused-vars": "off",
+      "react/display-name": "off",
+      "react/react-in-jsx-scope": "off",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
-  },
-)
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  }
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
+import { visualizer } from "rollup-plugin-visualizer";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
+import { compilerOptions } from "./tsconfig.paths.json";
+
+const resolveAlias = (paths: Record<string, string[]>) =>
+  Object.entries(paths).reduce(
+    (acc, [alias, path]) => {
+      acc[alias] = resolve(__dirname, ...path);
+      return acc;
+    },
+    {} as Record<string, string>,
+  );
+
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    cssMinify: true,
+    minify: true,
+  },
+  plugins: [
+    react(),
+    svgr(),
+    visualizer({
+      filename: "visualizer.html",
+      template: "treemap",
+      open: false,
+    }),
+  ],
+  resolve: {
+    alias: resolveAlias(compilerOptions.paths),
+  },
+});
